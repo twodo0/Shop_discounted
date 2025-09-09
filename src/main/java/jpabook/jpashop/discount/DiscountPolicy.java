55370cb114d5c924d78f2a62cfedf5d6e1ca9dab
twodo0 <dopal0426@gmail.com>
2025-09-09 16:05:08 +0900

    Update DiscountPolicy.java

--- src/main/java/jpabook/jpashop/discount/DiscountPolicy.java
+++ src/main/java/jpabook/jpashop/discount/DiscountPolicy.java
@@ -2,10 +2,7 @@ package jpabook.jpashop.discount;
 
 import jpabook.jpashop.domain.Member;
 
-/**
- * 할인 정책 인터페이스
- * @return 할인 금액(원)
- */
+// 할인 정책 인터페이스 
 public interface DiscountPolicy {
     int discount(Member member, int price);
 }
